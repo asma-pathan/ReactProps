d13706626ffe6ebee87ab9da0c590d2c7459fce9
asma-pathan <asmapathan752@gmail.com>
2022-11-30 16:52:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//import ClassComponent from './ClassComponent'
+import PropsExample from './PropsExample'
+
+function App(){
+  return(
+      <section>
+        <PropsExample name="asma" age="19"/>
+        <PropsExample name="hamini" age="19"/>
+        {/*<h1>You decide :):</h1>
+        <p>Always :)</p>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/e/e8/Chris_Hemsworth_by_Gage_Skidmore_2_%28cropped%29.jpg" width="30%" alt="pic1"/>
+        <ClassComponent/>*/}
+      </section>
+    );
+}
+export default App;
