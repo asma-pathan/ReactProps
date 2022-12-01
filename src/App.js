f9a7c62e98cd5a38fabdf259845bc4b93fd07b5d
asma-pathan <asmapathan752@gmail.com>
2022-12-01 16:48:55 +0530

    states&events

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 //import ClassComponent from './ClassComponent'
-import PropsExample from './PropsExample'
+//import PropsExample from './PropsExample'
+import FuncProps from './FuncProps'
+import StateExample from './StateExample'
+import DemoExample from './DemoExample'
+import Events from './Events'
 
 function App(){
   return(
       <section>
-        <PropsExample name="asma" age="19"/>
+      {/*  <PropsExample name="asma" age="19"/>
         <PropsExample name="hamini" age="19"/>
-        {/*<h1>You decide :):</h1>
+        <h1>You decide :):</h1>
         <p>Always :)</p>
         <img src="https://upload.wikimedia.org/wikipedia/commons/e/e8/Chris_Hemsworth_by_Gage_Skidmore_2_%28cropped%29.jpg" width="30%" alt="pic1"/>
         <ClassComponent/>*/}
-      </section>
+      <Events />
+    <DemoExample price="adsbnk"/> 
+    <StateExample/>
+    <h1>From functional component</h1>
+    <FuncProps carname="Range Rover" year="1996"/>
+    <FuncProps carname="BMW" year="1972"/>
+     </section>
+
     );
 }
 export default App;
---
+++ src/FuncProps.js
@@ -0,0 +1,10 @@
+function FuncProps(props){
+	return(
+		<div>
+		
+		{props.carname} {props.year}
+		
+		</div>
+		)
+}
+export default FuncProps
