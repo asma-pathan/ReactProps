5db0cc634ae91119ce07979f2c49f9df7a697178
asma-pathan <asmapathan752@gmail.com>
2022-12-02 16:50:18 +0530

    JSX

--- src/App.js
+++ src/App.js
@@ -4,22 +4,24 @@ import FuncProps from './FuncProps'
 import StateExample from './StateExample'
 import DemoExample from './DemoExample'
 import Events from './Events'
+import JSX from './JSX'
 
 function App(){
   return(
       <section>
-      {/*  <PropsExample name="asma" age="19"/>
+      <StateExample />
+      {/*  <JSX />
+      <PropsExample name="asma" age="19"/>
         <PropsExample name="hamini" age="19"/>
         <h1>You decide :):</h1>
         <p>Always :)</p>
         <img src="https://upload.wikimedia.org/wikipedia/commons/e/e8/Chris_Hemsworth_by_Gage_Skidmore_2_%28cropped%29.jpg" width="30%" alt="pic1"/>
-        <ClassComponent/>*/}
+        <ClassComponent/>
       <Events />
     <DemoExample price="adsbnk"/> 
-    <StateExample/>
     <h1>From functional component</h1>
     <FuncProps carname="Range Rover" year="1996"/>
-    <FuncProps carname="BMW" year="1972"/>
+    <FuncProps carname="BMW" year="1972"/>*/}
      </section>
 
     );
